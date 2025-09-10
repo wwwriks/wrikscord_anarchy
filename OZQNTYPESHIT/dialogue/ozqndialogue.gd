7213ae5cc2192468991fcb4287737470aa4a794d
LargeMitchell <109074768+LargeMitchell@users.noreply.github.com>
2025-09-10 10:56:45 +0200

    fixed centred buttons

--- OZQNTYPESHIT/dialogue/ozqndialogue.gd
+++ OZQNTYPESHIT/dialogue/ozqndialogue.gd
@@ -3,9 +3,12 @@ extends CanvasLayer
 @export var character_label: Label
 @export var dialogue_label: DialogueLabel
 @export var response_menu: DialogueResponsesMenu
+@export var animator: AnimationPlayer
 
 var current_text: DialogueResource
 var line: DialogueLine
+
+var in_dialogue: bool = false
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("progress_dialogue"):
@@ -20,9 +23,11 @@ func _input(_event: InputEvent) -> void:
 func _ready() -> void:
 	dialogue_label.finished_typing.connect(finished_typing)
 	response_menu.response_selected.connect(response_selected)
+	animator.animation_finished.connect(on_anim_finished)
 	hide()
 
 func start(dialogue_path: String = "uid://2b8xc8gk53r4") -> void:
+	in_dialogue = true
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	current_text = await OzqnLoader.load(dialogue_path, get_tree()) as DialogueResource
 	line = await current_text.get_next_dialogue_line("start")
@@ -30,7 +35,7 @@ func start(dialogue_path: String = "uid://2b8xc8gk53r4") -> void:
 	dialogue_label.visible_characters = 0
 	dialogue_label.dialogue_line = line
 	show()
-	dialogue_label.type_out()
+	animator.play("fade_in")
 
 func next(next_id: String = "") -> void:
 	if next_id:
@@ -47,9 +52,12 @@ func next(next_id: String = "") -> void:
 	dialogue_label.type_out()
 
 func end() -> void:
-	hide()
-	current_text = null
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	in_dialogue = false
+	animator.play("fade_out")
+	#hide()
+	#current_text = null
+	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func response_selected(response: DialogueResponse) -> void:
 	next(response.next_id)
@@ -57,3 +65,11 @@ func response_selected(response: DialogueResponse) -> void:
 
 func finished_typing() -> void:
 	response_menu.responses = line.responses
+
+func on_anim_finished(animation: StringName) -> void:
+	match animation:
+		"fade_out":
+			hide()
+			current_text = null
+		"fade_in":
+			dialogue_label.type_out()
