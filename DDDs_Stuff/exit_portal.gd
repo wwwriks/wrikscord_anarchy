2c672a7c445ede65916e3a7caa287ac51b25b496
ozqn <ozansomak@gmail.com>
2026-09-12 10:47:36 +0200

    Fixed dependency issue

--- DDDs_Stuff/exit_portal.gd
+++ DDDs_Stuff/exit_portal.gd
@@ -1,8 +1,9 @@
 extends Node3D
 
-@export var next_scene: PackedScene
+@export_file("*.tscn") var scene: String
 
 # D
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
-		get_tree().change_scene_to_packed(next_scene)
+		var level: PackedScene = load(scene)
+		get_tree().change_scene_to_packed(level)
